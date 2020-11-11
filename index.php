e2d5b98e2c33be8af8c5528f5f112a88d6c7d925
CsabaOrdog <ordog.csaba.0@gmail.com>
2020-11-11 15:58:12 +0100

    start

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css" />
+    <script src="bootstrap/jquery.min.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
+    <title>Weboldal</title>
+</head>
+<body>
+    <div class="jumbotron">Fejlesztés alatt</div>
+</body>
+</html>
