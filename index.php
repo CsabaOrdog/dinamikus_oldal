5178327e248b119593f56cd36cad2d6d36a16d9a
CsabaOrdog <ordog.csaba.0@gmail.com>
2020-11-13 10:18:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap/bootstrap.min.css" />
-    <script src="bootstrap/jquery.min.js"></script>
-    <script src="bootstrap/bootstrap.min.js"></script>
-    <title>Weboldal</title>
-</head>
-<body>
-    <div class="jumbotron">Fejlesztés alatt</div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="bootstrap/bootstrap.min.css" />
+        <script src="bootstrap/jquery.min.js"></script>
+        <script src="bootstrap/bootstrap.min.js"></script>
+        <title>Weboldal</title>
+    </head>
+    <body>
+        <div class="jumbotron">Fejlesztés alatt</div>
+    </body>
 </html>
