ed03e5276018360013dcc877645e2247bcacab6c
CsabaOrdog <ordog.csaba.0@gmail.com>
2020-11-13 10:56:23 +0100

    css/background

--- index.php
+++ index.php
@@ -4,11 +4,19 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="bootstrap/bootstrap.min.css" />
+        <link rel="stylesheet" href="styles.css" />
         <script src="bootstrap/jquery.min.js"></script>
         <script src="bootstrap/bootstrap.min.js"></script>
         <title>Weboldal</title>
     </head>
     <body>
-        <div class="jumbotron">Fejlesztés alatt</div>
+        <div class="container">
+            <div class="row" id="cimsor">
+                <div class="col text-center">
+                    <h1 id="cim">Valami jobb cim</h1>
+                </div>
+            </div>
+
+        </div>
     </body>
 </html>
